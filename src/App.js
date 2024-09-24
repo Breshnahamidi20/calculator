19110c5eb8d8eef821b69c42a432c08290bfe3a6
unknown <breshna2004@gmail.com>
2024-09-24 17:39:55 +0430

    install react redux twlind css delete some extra files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Calculator from './components/Calculator';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import './index.css';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <Calculator />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
